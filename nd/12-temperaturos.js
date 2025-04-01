37685d26f83f9cc7e89cab3edf1e0c0c05567ec0
Aura <a.pozeraite@gmail.com>
2025-04-01 18:17:03 +0300

    Temperaturos

---
+++ nd/12-temperaturos.js
@@ -0,0 +1,85 @@
+console.log('a) Parasyti funkcija, kuri apskaiciuotu norimos dienos vidutine temperatura');
+
+let pirmadienis = [0, 3, 5, 8, 6];
+let antradienis = [-5, -3, -2, 0, 1];
+let treciadienis = [2, 5, 8, 10, 7];
+let ketvirtadienis = [5, 8, 11, 9, 6];
+let penktadienis = [6, 8, 11, 9, 5];
+let sestadienis = [6, 9, 12, 15, 10];
+let sekmadienis = [9, 12, 16, 14, 11];
+
+
+function vidutineTemperatura(diena) {
+    let vidurkis = 0;
+    for ( i = 0; i < diena.length; i++) {       
+        vidurkis += diena[i];
+    }
+    return vidurkis / diena.length;
+}
+
+console.log(vidutineTemperatura(pirmadienis));
+console.log(vidutineTemperatura(antradienis));
+console.log(vidutineTemperatura(treciadienis));
+console.log(vidutineTemperatura(ketvirtadienis));
+console.log(vidutineTemperatura(penktadienis));
+console.log(vidutineTemperatura(sestadienis));
+console.log(vidutineTemperatura(sekmadienis));
+
+console.log('b) Parasyti funkcija, kuri apskaiciuotu norimos dienos auksciausia temperatura');
+
+let didziausia = -Infinity;
+
+function didziausiaTemperatura (diena) {
+    let didziausia = -Infinity;
+         for ( i = 0; i < diena.length; i++) {
+            if (diena[i] > didziausia) {
+                didziausia = diena[i];
+            }
+         }
+         return didziausia;
+}
+
+console.log(didziausiaTemperatura(pirmadienis));
+console.log(didziausiaTemperatura(antradienis));
+console.log(didziausiaTemperatura(treciadienis));
+console.log(didziausiaTemperatura(ketvirtadienis));
+console.log(didziausiaTemperatura(penktadienis));
+console.log(didziausiaTemperatura(sestadienis));
+console.log(didziausiaTemperatura(sekmadienis));
+
+console.log('3) Parasyti funkcija, kuri apskaiciuotu norimos dienos zemiausia temperatura');
+
+let maziausia = Infinity;
+
+function maziausiaTemperatura (diena) {
+    let maziausia = Infinity;
+         for ( i = 0; i < diena.length; i++) {
+            if (diena[i] < maziausia) {
+                maziausia = diena[i];
+            }
+         }
+         return maziausia;
+}
+
+console.log(maziausiaTemperatura(pirmadienis));
+console.log(maziausiaTemperatura(antradienis));
+console.log(maziausiaTemperatura(treciadienis));
+console.log(maziausiaTemperatura(ketvirtadienis));
+console.log(maziausiaTemperatura(penktadienis));
+console.log(maziausiaTemperatura(sestadienis));
+console.log(maziausiaTemperatura(sekmadienis));
+
+console.log('e) Parasyti funkcija, kuri apskaiciuotu visos savaites vidutine temperatura');
+
+let a = [0, 3, 5, 8, 6];
+let b = [-5, -3, -2, 0, 1];
+let c = [2, 5, 8, 10, 7];
+let d = [5, 8, 11, 9, 6];
+let e = [6, 8, 11, 9, 5];
+let f = [6, 9, 12, 15, 10];
+let g = [9, 12, 16, 14, 11];
+
+
+
+
+
